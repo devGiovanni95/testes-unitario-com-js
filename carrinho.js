2e6082feec74cf393b041c15a615d104911c71e6
devGiovanni95 <giovanni.devJava@hotmail.com>
2024-08-01 22:06:18 -0300

    more tests

--- carrinho.js
+++ carrinho.js
@@ -1,39 +1,37 @@
 class Carrinho {
-    constructor(){
-        this.itens = []
-        this.subtotal = null
-        this.frete = null
-        this.total = null
+  constructor() {
+    this.itens = [];
+    this.subtotal = null;
+    this.frete = null;
+    this.total = null;
+  }
+
+  adiciona(item) {
+    this.itens.push(item);
+  }
+
+  adicionaFrete(valor) {
+    this.frete = valor;
+  }
+
+  calculaTotal() {
+    this.subtotal = this.itens.reduce((acum, item) => acum + item.pegaValorTotalItem(), 0);
+    return this.subtotal + this.frete;
+  }
+
+  finalizaCompra() {
+    if (this.itens.length === 0) {
+      throw new Error('Carrinho de compras vazios');
     }
 
-    adiciona(item){
-        this.itens.push(item);
-    }
+    this.total = this.calculaTotal();
 
-    adicionaFrete(valor){
-        this.frete(valor);
-    }
-
-    calculaTotal(){
-        this.subtotal = this.itens.reduce((acum, item) => acum);
-        return this.subtotal + this.frete;
-    }
-
-    finalizaCompra() {
-        if(this.itens.length === 0){
-            throw new Error('Carrinho de compras vazios')
-        }
-
-        this.total = this.calculaTotal();
-
-        return {
-            subtotal: this.subtotal,
-            frete: this.frete,
-            total: this.total,            
-        }
-    }
-
-    
+    return {
+      subtotal: this.subtotal,
+      frete: this.frete,
+      total: this.total,
+    };
+  }
 }
 
-module.exports = {Carrinho}
+module.exports = { Carrinho };
